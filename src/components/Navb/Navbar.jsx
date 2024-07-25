c60694c4f8ed3db0d62831f0fa8315edb8bc99d6
aj <balajivijay679@gmail.com>
2024-07-25 21:16:41 +0530

    c

--- src/components/Navb/Navbar.jsx
+++ src/components/Navb/Navbar.jsx
@@ -14,20 +14,20 @@ function Navbar(){
 
                 <div className="bgg">
                 <ul>
-                        <a href="/" target="blank"><li>Home</li></a>
-                       <a href="#Tech" target="blank" style={{textDecoration:"line-through"}} target="blank"> <li>Techstack</li></a>
-                        <a href="#Project" target="blank" style={{textDecoration:"line-through"}} target="blank"><li>Project</li></a>
-                        <a href="/contact" target="blank"><li>contact</li></a>
+                        <a href="/" target="_blank"><li>Home</li></a>
+                       <a href="#Tech" target="_blank" style={{textDecoration:"line-through"}} > <li>Techstack</li></a>
+                        <a href="#Project" target="_blank" style={{textDecoration:"line-through"}}><li>Project</li></a>
+                        <a href="/contact" target="_blank"><li>contact</li></a>
                     </ul>
             </div>
                 </div>
 
                 <div className="lists">
                     <ul>
-                        <a href="/" target="blank"><li>Home</li></a>
-                       <a href="#Tech" target="blank" style={{textDecoration:"line-through"}} target="blank"> <li>Techstack</li></a>
-                        <a href="#Project" target="blank" style={{textDecoration:"line-through"}} target="blank"><li>Project</li></a>
-                        <a href="/contact" target="blank"><li>contact</li></a>
+                        <a href="/" target="_blank"><li>Home</li></a>
+                       <a href="#Tech" target="_blank" style={{textDecoration:"line-through"}} target="blank"> <li>Techstack</li></a>
+                        <a href="#Project" target="_blank" style={{textDecoration:"line-through"}} target="blank"><li>Project</li></a>
+                        <a href="/contact" target="_blank"><li>contact</li></a>
                     </ul>
                 </div>
             </div>
